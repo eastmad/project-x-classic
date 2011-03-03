d40b8075ff1d577c123873509c628e27b5696196
David Eastman <david.eastman@bt.com>
2011-03-03 02:12:02 +0000

    talk text

---
+++ long_text.rb
@@ -0,0 +1,12 @@
+class LongText
+    
+    @@txt = {:war =>
+"When the war came, it started with one unexpected act of independence. When the war stopped,\
+it ended with the collapse of both economies. The Earth elite left their listening posts and \
+their system of government."
+    }
+    
+    def self.txt access
+       @@txt[access] 
+    end
+end
--- test_u2.rb
+++ test_u2.rb
@@ -1,14 +1,8 @@
-Shoes.app do
-  @s0 = stack do
-    background red
-    100.times do
-      para "yay"
-    end
-  end
-  @s = stack do
-    style(:attach => Window, :top => height - 100)
-    background lightblue
-    para app.width
-    para app.height
-  end
-end
+ Shoes.app :title => "The Owner" do
+   button "Pop up?" do
+     window :hidden => true do
+       para "Okay, popped up from #{owner}"
+     end
+   end
+ end
+
